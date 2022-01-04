ccd7ad6a969460bb9a4b74833fc013b75b3f18c0
raviv <raviv@Cloudtech41.CTS.CLOUDTECHSOFT.COM>
2022-01-04 19:04:09 +0530

    second commit

---
+++ CLMebAddNew/src/test/java/com/generic/excel/Excel.java
@@ -0,0 +1,30 @@
+package com.generic.excel;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class Excel 
+{
+	public static String getCellData(String xlPath,String sheetName,int rowNum, int cellNum) 
+	{		
+		try 
+		{
+			FileInputStream fis = new FileInputStream(xlPath);
+			Workbook wb = WorkbookFactory.create(fis);
+			Sheet s = wb.getSheet(sheetName);
+			String v = s.getRow(rowNum).getCell(cellNum).getStringCellValue();
+			
+			return v;
+		}
+			
+		catch(Exception e)
+		{
+			return "";
+		}
+		
+	}
+
+}
